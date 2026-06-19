3b6c0ef048df8b52acb2734448c724ccdfa4c90e
Hector Oreno <heoreno@gmail.com>
2026-06-19 12:25:16 -0600

    Frontend premium finalizado e inicio backend Node.js

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const pool = require('./src/config/db');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+
+        res.json({
+            success: true,
+            server_time: result.rows[0].now
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Error conectando con PostgreSQL'
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en puerto ${PORT}`);
+});
--- js/main.js
+++ js/main.js
@@ -8,7 +8,54 @@
     document.getElementById('ejecutivoIncludes').innerHTML =
       '<strong>Todos nuestros platillos incluyen:</strong> ' + MENU_EJECUTIVO.incluye;
 
+    const menuHoy = document.getElementById('menuHoy');
+
+    const toggleSemanaBtn = document.getElementById('toggleSemanaBtn');
+
     const diasGrid = document.getElementById('diasGrid');
+    diasGrid.classList.add('hidden');
+
+    const menuDelDia =
+      MENU_EJECUTIVO.dias.find(d => d.esHoy);
+
+    if (menuDelDia) {
+
+      const acompHoy =
+        menuDelDia.acomp
+          .map(a => `<span>${a}</span>`)
+          .join('');
+
+      menuHoy.innerHTML = `
+        <div class="menu-hoy-card">
+
+          <div class="menu-hoy-img">
+            ${menuDelDia.emojis}
+          </div>
+
+          <div class="menu-hoy-body">
+
+            <div class="menu-hoy-badge">
+              🔥 MENÚ DE HOY
+            </div>
+
+            <h3 class="menu-hoy-title">
+              ${menuDelDia.plato}
+            </h3>
+
+            <div class="menu-hoy-price">
+              Q${menuDelDia.precio}
+            </div>
+
+            <div class="menu-hoy-acomp">
+              ${acompHoy}
+            </div>
+
+          </div>
+
+        </div>
+      `;
+    }
+
     MENU_EJECUTIVO.dias.forEach((d, i) => {
       const esHoy = i === DIA_EN_MENU;
       const card = document.createElement('div');
@@ -71,6 +118,52 @@
       menuGrid.appendChild(div);
     });
 
+
+    toggleSemanaBtn.addEventListener('click', () => {
+
+      diasGrid.classList.toggle('hidden');
+
+      const cards =
+        document.querySelectorAll('.dia-card');
+
+      if (
+        !diasGrid.classList.contains('hidden')
+      ) {
+
+        cards.forEach((card, index) => {
+
+          setTimeout(() => {
+
+            card.classList.add('visible');
+
+          }, index * 100);
+
+        });
+
+      } else {
+
+        cards.forEach(card => {
+
+        card.classList.remove('visible');
+
+        });
+
+      }
+
+      if (
+        diasGrid.classList.contains('hidden')
+      ) {
+
+        toggleSemanaBtn.textContent =  '▼ Ver menú semanal';
+
+      } else {
+
+        toggleSemanaBtn.textContent = '▲ Ocultar menú semanal';
+      }
+
+    });
+
+
     // ── Renderizar sucursales ──
     const branchesGrid = document.getElementById('branchesGrid');
     SUCURSALES.forEach(s => {
@@ -93,17 +186,18 @@
       <div class="branch-info">
 
         <div class="branch-row">
-          <span class="ico">📍</span>
+          <i class="fa-solid fa-location-dot ico"></i>
           <span>${s.direccion}</span>
+          
         </div>
 
         <div class="branch-row">
-          <span class="ico">🕐</span>
+          <i class="fa-solid fa-clock ico"></i>
           <span>${s.horario}</span>
         </div>
 
       <div class="branch-row">
-        <span class="ico">📞</span>
+        <i class="fa-solid fa-phone ico"></i>
         <span>${s.telefono}</span>
     </div>
 
@@ -112,18 +206,17 @@
   
 
     <a
-      href="https://maps.google.com"
+      href="https://www.google.com/maps/place/TEXACO+%E2%80%A2+Santa+Luc%C3%ADa/@14.334056,-91.008894,17z/data=!4m6!3m5!1s0x8589271db6bce279:0xf52713cc9addfa85!8m2!3d14.334031!4d-91.008881!16s%2Fg%2F11v5ft7n2t"
       target="_blank"
       class="branch-map">
 
-      🗺️ Cómo llegar
+      <i class="fa-solid fa-route"></i>Cómo llegar
 
     </a>
 
   `;
       branchesGrid.appendChild(div);
     });
-
 
 
     // ── Filtro de menú ──
@@ -135,10 +228,266 @@
       });
     }
 
-    // ── Nav móvil ──
-    document.getElementById('navToggle').addEventListener('click', () => {
-      document.getElementById('navLinks').classList.toggle('open');
+    const menuCards = document.querySelectorAll('.menu-explore .menu-card');
+
+    const menuActiveFilter =
+      document.getElementById('menuActiveFilter');
+
+    const currentCategory =
+      document.getElementById('currentCategory');
+
+    const showAllBtn =
+      document.getElementById('showAllBtn');
+
+      let activeCategory = null;
+
+    const menuExplore =
+      document.getElementById('menuExplore'); 
+      
+    const menuShowAllCard =
+      document.getElementById('menuShowAllCard');
+
+    menuCards.forEach(card => {
+
+      card.addEventListener('click', () => {
+
+        const categoria = card.dataset.cat;
+
+        if (activeCategory === categoria) {
+        
+
+        activeCategory = null;
+
+        card.classList.remove('active');
+
+        menuGrid.classList.remove('active');
+
+        menuActiveFilter.classList.remove('active');
+
+        menuExplore.classList.remove('hidden');
+
+        return;
+        }
+
+      activeCategory = categoria;
+      console.log('ACTIVA:', activeCategory);
+
+      menuGrid.classList.add('active');
+
+      menuExplore.classList.add('hidden');
+
+        menuCards.forEach(c =>
+          c.classList.remove('active')
+        );
+
+        card.classList.add('active');
+
+        menuActiveFilter.classList.add('active');
+
+        if (categoria === 'desayuno') {
+
+          currentCategory.textContent = 'Desayunos';
+
+          document.querySelectorAll('.menu-item')
+            .forEach(item => {
+
+              item.style.display =
+                item.dataset.cat === 'desayuno'
+                ? ''
+                : 'none';
+            });
+          }
+
+        if (categoria === 'especial') {
+
+          currentCategory.textContent =
+            'Especialidades';
+
+          document.querySelectorAll('.menu-item')
+            .forEach(item => {
+
+              const cat = item.dataset.cat;
+
+            item.style.display =
+              (
+                cat === 'especial' ||
+                cat === 'entrada' ||
+                cat === 'pasta' ||
+                cat === 'sandwich' ||
+                cat === 'infantil'
+              )
+              ? ''
+              : 'none';
+            });
+        }
+
+        if (categoria === 'bebidas') {
+
+          currentCategory.textContent =
+          'Bebidas';
+
+          document.querySelectorAll('.menu-item')
+            .forEach(item => {
+
+              const cat = item.dataset.cat;
+
+              item.style.display =
+                (
+                  cat === 'bebida_caliente' ||
+                  cat === 'bebida_fria'
+                )
+                ? ''
+                : 'none';
+            });
+        }
+
+        if (categoria === 'postres') {
+
+          currentCategory.textContent =
+            'Postres';
+
+          document.querySelectorAll('.menu-item')
+            .forEach(item => {
+
+              item.style.display =
+                item.dataset.cat === 'postre'
+                ? ''
+                : 'none';
+            });
+         }
+
+      });
+
+    });  
+
+    showAllBtn.addEventListener('click', () => {
+
+      
+
+      document.querySelectorAll('.menu-item')
+        .forEach(item => {
+
+          item.style.display = '';
+        });
+
+    activeCategory = null;
+      
+      menuCards.forEach(card =>
+        card.classList.remove('active')
+      );
+
+      menuActiveFilter.classList.remove('active');
+
+      menuExplore.classList.remove('hidden');
+
     });
+
+
+    currentCategory.addEventListener('click', () => {
+
+      activeCategory = null;
+
+      document.querySelectorAll('.menu-item')
+        .forEach(item => {
+
+        item.style.display = '';
+      });
+
+      menuCards.forEach(card =>
+        card.classList.remove('active')
+      );
+
+    menuGrid.classList.remove('active');
+
+    menuActiveFilter.classList.remove('active');
+
+    menuExplore.classList.remove('hidden');
+
+    });
+
+    menuShowAllCard.addEventListener('click', () => {
+
+      document.querySelectorAll('.menu-item')
+        .forEach(item => {
+
+          item.style.display = '';
+        });
+
+      menuGrid.classList.add('active');
+
+      menuExplore.classList.add('hidden');
+
+      currentCategory.textContent =
+      '← Todos';
+
+      menuActiveFilter.classList.add('active');
+
+    });
+
+
+
+
+
+    // ── Nav móvil ──
+
+    const navToggle = document.getElementById('navToggle');
+    const navLinks = document.getElementById('navLinks');
+    const navClose = document.getElementById('navClose');
+    const navOverlay = document.getElementById('navOverlay');
+
+    navToggle.addEventListener('click', () => {
+
+      navLinks.classList.add('open');
+
+      navOverlay.classList.add('active');
+
+    });
+
+    navClose.addEventListener('click', () => {
+
+      navLinks.classList.remove('open');
+
+      navOverlay.classList.remove('active');
+
+    });
+
+    // Cerrar menú al seleccionar una opción
+  
+    document.querySelectorAll('#navLinks a').forEach(link => {
+
+    link.addEventListener('click', () => {
+
+      setTimeout(() => {
+
+        navLinks.classList.remove('open');
+        navOverlay.classList.remove('active');
+
+      }, 150);
+
+    });
+
+  });
+
+
+    // Cerrar al tocar fuera del Drawer
+
+  document.addEventListener('click', (e) => {
+
+    const menuOpen = navLinks.classList.contains('open');
+
+    if (
+      menuOpen &&
+      !navLinks.contains(e.target) &&
+      !navToggle.contains(e.target)
+    )  {
+
+      navLinks.classList.remove('open');
+      navOverlay.classList.remove('active');
+
+    }
+
+  });
+
 
     // ── Nav sombra al scroll ──
     window.addEventListener('scroll', () => {
@@ -257,9 +606,50 @@
       }
     });
 
+    
       // Parar al pasar el mouse encima (mejor UX)
       document.querySelector('.hero').addEventListener('mouseenter', () => clearInterval(timer));
       document.querySelector('.hero').addEventListener('mouseleave', () => {
         timer = setInterval(() => goTo(current + 1), DELAY);
       });
+
+
+      const revealElements =
+        document.querySelectorAll('.reveal');
+
+      const revealObserver =
+        new IntersectionObserver(
+
+        entries => {
+
+          entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+              entry.target.classList.add('revealed');
+
+            } else {
+
+              entry.target.classList.remove('revealed');
+
+            }
+
+          });
+
+        },
+
+        {
+          threshold: 0.25
+        }
+
+      );
+
+      revealElements.forEach(el => {
+        revealObserver.observe(el);
+      });
+
+
+
+
+
     })();
